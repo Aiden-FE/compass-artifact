551bb323e505573b645cca8a5e4e9cb9b21738b0
Aiden-FE <Aiden_FE@outlook.com>
2023-06-16 15:16:43 +0800

    fix: ci error

--- libraries/commander/.eslintrc.cjs
+++ libraries/commander/.eslintrc.cjs
@@ -8,13 +8,9 @@ module.exports = {
   parserOptions: {
     project: './tsconfig.json',
   },
-  plugins: ['prettier'],
   extends: [
     // typescript使用此配置
     '@compass-aiden/eslint-config/ts',
     'plugin:prettier/recommended',
   ],
-  rules: {
-    'prettier/prettier': 'error',
-  },
 };
